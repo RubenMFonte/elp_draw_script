527870db94229436e10b8930d5c276f03ce263ea
Ruben Fonte <ruben.m.fonte@gmail.com>
2023-05-29 11:36:14 +0100

    Add extractColor

--- src/main/kotlin/DS_Interpreter.kt
+++ src/main/kotlin/DS_Interpreter.kt
@@ -2,10 +2,11 @@ import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.Rectangle
+import java.awt.Shape
 import javax.swing.JComponent
 import javax.swing.JFrame
 
-class Main : JComponent() {
+class Window : JComponent() {
 
     override fun paintComponent(graphics: Graphics) {
 
@@ -25,9 +26,13 @@ class Main : JComponent() {
 
 }
 
+class InstructionBlock {
+    var variables = HashMap<String, Int>()
+}
+
 class DS_Interpreter : JComponent() {
 
-    data class DrawObject(val shape: DS_ASTParser.Shape, val color: DS_ASTParser.Color)
+    data class DrawObject(val shape: DS_ASTParser.Shape, val color: DS_ASTParser.Color, val drawMode: DS_ASTParser.DrawMode)
 
     var constants = HashMap<String, Int>()
     var currentDrawMode = DS_ASTParser.DrawMode.LINE
@@ -54,16 +59,19 @@ class DS_Interpreter : JComponent() {
         val script = ast_parser.parseScript(file)
 
         getConstants(script.constants)
+        runInstructions(script.logic, hashMapOf<String, Int>())
 
         print(constants)
 
         frame.setSize(width, height)
 
-        frame.add(Main())
+        frame.add(this)
         frame.isVisible = true
     }
 
-    private fun runInstructions(instructions: DS_ASTParser.InstructionBlock) {
+    private fun runInstructions(instructions: DS_ASTParser.InstructionBlock, variables: HashMap<String, Int>) {
+
+        var localVariables: HashMap<String, Int> = variables
 
         for(instruction in instructions.instructions) {
 
@@ -72,9 +80,22 @@ class DS_Interpreter : JComponent() {
                 currentColor = instruction.color
             }
             else if(instruction is DS_ASTParser.Shape) {
+
                 objectsToDraw.add(
-                    DrawObject( instruction, currentColor)
+                    DrawObject( instruction, currentColor, currentDrawMode)
                 )
+            }
+            else if(instruction is DS_ASTParser.For) {
+
+                var start = calculateValue(instruction.interval.start)
+                val end = calculateValue(instruction.interval.end)
+
+                localVariables[instruction.iterator] = start
+
+                for(i in start..end)        {
+                    
+                }
+
             }
         }
     }
@@ -82,13 +103,23 @@ class DS_Interpreter : JComponent() {
     private fun getConstants(assignments: DS_ASTParser.AssignmentBlock) {
 
         for(assignment in assignments.assignments) {
+            constants[assignment.identifier] = calculateValue(assignment.value)
+        }
+    }
 
-            if(assignment.value is DS_ASTParser.BinaryExpression) {
-                constants[assignment.identifier] = calculateExpression(assignment.value)
+    private fun calculateValue(value: DS_ASTParser.Value) : Int {
+
+        return when (value) {
+            is DS_ASTParser.BinaryExpression -> {
+                calculateExpression(value)
             }
-            else if(assignment.value is DS_ASTParser.Num) {
-                constants[assignment.identifier] = assignment.value.value
+            is DS_ASTParser.Constant -> {
+                constants[value.identifier] ?: 0
             }
+            is DS_ASTParser.Num -> {
+                value.value
+            }
+            else -> 0
         }
     }
 
@@ -133,8 +164,67 @@ class DS_Interpreter : JComponent() {
 
         for(drawObject in objectsToDraw) {
 
-            g.color = Color()
+
+            print("drawObject")
+
+            var red = 0
+            var green = 0
+            var blue = 0
+
+            red = calculateValue( drawObject.color.values.first() )
+
+            if(drawObject.color.values.size > 1) {
+                green = calculateValue( drawObject.color.values[1] )
+                blue = calculateValue( drawObject.color.values[2] )
+            }
+            else {
+                green = red;
+                blue = red;
+            }
+
+            g.color = Color( red,green,blue )
+
+            var shape = getShape(drawObject.shape) as Rectangle
+
+            when(drawObject.drawMode) {
+                DS_ASTParser.DrawMode.FILL -> g.fillRect(shape.x, shape.y, shape.width, shape.height)
+                DS_ASTParser.DrawMode.LINE -> g.draw(shape)
+            }
+
         }
 
     }
+
+    private fun extractColor(color: DS_ASTParser.Color)  : Color  {
+
+        var red = 0
+        var green = 0
+        var blue = 0
+
+        red = calculateValue( color.values.first() )
+
+        if(color.values.size > 1) {
+            green = calculateValue( color.values[1] )
+            blue = calculateValue( color.values[2] )
+        }
+        else {
+            green = red;
+            blue = red;
+        }
+
+        return Color(red, green, blue)
+    }
+
+    private fun getShape(shape: DS_ASTParser.Shape): Shape {
+
+        return if(shape is DS_ASTParser.Rectangle) {
+            Rectangle(
+                calculateValue(shape.position.x),
+                calculateValue(shape.position.y),
+                calculateValue(shape.dimension.x),
+                calculateValue(shape.dimension.y)
+            )
+        } else Rectangle(0,0,0,0)
+    }
+
 }
